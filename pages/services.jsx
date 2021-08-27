7b8e7a73c29cd8be8da8a0b1051b91fe53189af1
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2021-08-27 09:12:40 -0500

    Create services.jsx

---
+++ pages/services.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "../src/layouts/Container";
+
+const Services = () => {
+	return (
+		<Container title="services">
+			<h1>services</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa eos
+				accusantium cum aliquam, in quae eveniet dolorem magnam. Suscipit,
+				mollitia! Consequuntur nemo cum aliquid fugit suscipit cumque soluta
+				recusandae distinctio?
+			</p>
+		</Container>
+	);
+};
+
+export default Services;
