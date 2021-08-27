98b380d29d2411d378d3006fa4a09ebc708acb22
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2021-08-27 09:11:13 -0500

    Create about.jsx

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../src/layouts/Container";
+
+const About = () => {
+	return (
+    <Container title="about">
+      <h1>about</h1>
+    </Container>
+  );
+};
+
+export default About;
